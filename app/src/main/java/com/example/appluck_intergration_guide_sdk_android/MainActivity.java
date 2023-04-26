e8e04ea07686a587e8656885292642d6f28e94fd
luoxu <962526394@qq.com>
2023-04-26 18:06:57 +0800

    1.Get ad slot view

--- app/src/main/java/com/example/appluck_intergration_guide_sdk_android/MainActivity.java
+++ app/src/main/java/com/example/appluck_intergration_guide_sdk_android/MainActivity.java
@@ -1,23 +1,16 @@
 package com.example.appluck_intergration_guide_sdk_android;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.appluck.sdk.AppLuckSDK;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,33 +22,64 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button btnActivity2 = findViewById(R.id.btn_activity2);
-        btnActivity2.setOnClickListener((v) -> {
+
+        showAppLuckPlacement();
+
+        findViewById(R.id.btn_activity2).setOnClickListener((v) -> {
             final Intent intent = new Intent(this, MainActivity2.class);
             startActivity(intent);
         });
         btnOpen = findViewById(R.id.btn_open);
+        btnOpen.setVisibility(View.GONE);
         btnOpen.setOnClickListener((v) -> {
             //插屏打开
             AppLuckSDK.openInteractiveAds(MyApplication.APPLUCK_PLACEMENT_ID, 1, 1);
         });
-        btnOpen.setVisibility(View.GONE);
-        final Button btnShow = findViewById(R.id.btn_show);
-        btnShow.setOnClickListener((v) -> {
+        findViewById(R.id.btn_show).setOnClickListener((v) -> {
             //APPLUCK show
-            AppLuckSDK.showInteractiveEntrance(this, MyApplication.APPLUCK_PLACEMENT_ID, 600, 600, 1);
+            AppLuckSDK.showInteractiveEntrance(this, MyApplication.APPLUCK_PLACEMENT_ID, 1);
         });
-        final Button btnHide = findViewById(R.id.btn_hide);
-        btnHide.setOnClickListener((v) -> {
+        findViewById(R.id.btn_hide).setOnClickListener((v) -> {
             //APPLUCK hide
-            AppLuckSDK.hideInteractiveEntrance(this,MyApplication.APPLUCK_PLACEMENT_ID);
+            AppLuckSDK.hideInteractiveEntrance(this, MyApplication.APPLUCK_PLACEMENT_ID);
         });
-        showAppLuckPlacement();
+        findViewById(R.id.btn_show2).setOnClickListener(view -> {
+            //APPLUCK show2
+            AppLuckSDK.showInteractiveEntrance(this, MyApplication.APPLUCK_PLACEMENT_ID2, 1);
+        });
+        findViewById(R.id.btn_hide2).setOnClickListener(view -> {
+            //APPLUCK hide2
+            AppLuckSDK.hideInteractiveEntrance(this, MyApplication.APPLUCK_PLACEMENT_ID2);
+        });
     }
 
     public void showAppLuckPlacement() {
+        //通过坐标指定到屏幕位置
+        /*if (AppLuckSDK.isPlacementReady(MyApplication.APPLUCK_PLACEMENT_ID)) {
+            AppLuckSDK.showInteractiveEntrance(this, MyApplication.APPLUCK_PLACEMENT_ID, 100, 600, 1);
+            btnOpen.setVisibility(View.VISIBLE);
+            return;
+        }*/
+
+        //通过获取SDK View,加到自己的Layout上
         if (AppLuckSDK.isPlacementReady(MyApplication.APPLUCK_PLACEMENT_ID)) {
-            AppLuckSDK.showInteractiveEntrance(this, MyApplication.APPLUCK_PLACEMENT_ID, 600, 600, 1);
+            LinearLayout llAddView = findViewById(R.id.ll_add_view);
+            View iconView = AppLuckSDK.showInteractiveEntrance(this, MyApplication.APPLUCK_PLACEMENT_ID, 1);
+            View iconView2 = AppLuckSDK.showInteractiveEntrance(this, MyApplication.APPLUCK_PLACEMENT_ID2, 1);
+            if (iconView != null) {
+                if (iconView.getParent() != null) {
+                    ViewGroup viewGroup = (ViewGroup) iconView.getParent();
+                    viewGroup.removeView(iconView);
+                }
+                if (iconView2.getParent() != null) {
+                    ViewGroup viewGroup = (ViewGroup) iconView2.getParent();
+                    viewGroup.removeView(iconView2);
+                }
+                iconView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                iconView2.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                llAddView.addView(iconView);
+                llAddView.addView(iconView2);
+            }
             btnOpen.setVisibility(View.VISIBLE);
             return;
         }
